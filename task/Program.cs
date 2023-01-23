72e09b026c09c65e01da23c0c2c0a0e5d23f0477
«Artur» <art13471118@gmail.com>
2023-01-23 22:07:39 +0300

    программа согласно итогового задания

---
+++ task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[5] { "Hello", "2", "world", ":-)", "Ufa" };
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] arr1, string[] arr2)
+{
+    int j = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+            arr2[j] = arr1[i];
+        j++;
+    }
+}
+
+void PrintArray(string[] arr3)
+{
+    for (int i = 0; i < arr3.Length; i++)
+    {
+        Console.Write($"{arr3[i]} ");
+    }
+}
+
+SecondArray(array1, array2);
+PrintArray(array2);
